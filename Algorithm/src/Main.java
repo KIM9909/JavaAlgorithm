fbf13c73506f37048fcbf5afb9d1640476680087
kimhongbeom <fkdldj48@naver.com>
2025-01-08 09:09:42 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -3,9 +3,25 @@ import java.io.*;
 
 public class Main {
     static int N, M;
-    static int A, B, C;
-    static boolean[] visited;
-    static ArrayList<ArrayList<int[]>> graph;
+    static int num, A, B;
+    static int[] parent;
+    static ArrayList<int[]> graph;
+
+    static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -14,57 +30,49 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N+1];
         graph = new ArrayList<>();
+
+        parent = new int[N+1];
         for (int i=0; i<=N; i++) {
-            graph.add(new ArrayList<>());
+            parent[i] = i;
         }
 
         for (int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
+            num = Integer.parseInt(st.nextToken());
             A = Integer.parseInt(st.nextToken());
             B = Integer.parseInt(st.nextToken());
-            C = Integer.parseInt(st.nextToken());
-            graph.get(A).add(new int[] {B, C});
-            graph.get(B).add(new int[] {A, C});
+            graph.add(new int[] {num, A, B});
         }
 
-        long res = prim();
-        System.out.println(res);
-    }
+        StringBuilder sb = new StringBuilder();
 
-    static long prim() {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        pq.offer(new int[] {1, 0});
+        for (int[] temp : graph) {
+            int num = temp[0];
+            int a = temp[1];
+            int b = temp[2];
 
-        long totalWeight = 0;
-        long maxWeight = 0;
-
-        while (!pq.isEmpty()) {
-            int[] current = pq.poll();
-            int vertex = current[0];
-            int weight = current[1];
-
-            if (visited[vertex]) {
-                continue;
-            }
-
-            visited[vertex] = true;
-            totalWeight += weight;
-
-            if (maxWeight < weight) {
-                maxWeight = weight;
-            }
-
-            for (int[] next : graph.get(vertex)) {
-                int nextVertx = next[0];
-                int nextWeight = next[1];
-
-                if (!visited[nextVertx]) {
-                    pq.offer(new int[] {nextVertx, nextWeight});
+            if (num == 0 && find(a) != find(b)) {
+                union(a, b);
+            } else if (num == 1) {
+                boolean isSame = true;
+                if (!check(a, b)) {
+                    isSame = false;
+                }
+                if (!isSame) {
+                    sb.append("NO").append('\n');
+                } else {
+                    sb.append("YES").append('\n');
                 }
             }
         }
-        return totalWeight - maxWeight;
+        System.out.println(sb);
+    }
+    static boolean check(int a, int b) {
+        if (find(a) != find(b)) {
+            return false;
+        } else {
+            return  true;
+        }
     }
 }
