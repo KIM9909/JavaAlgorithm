d8cce02f2e114dc76ce818a7657e734a6e7cdc97
kimhongbeom <fkdldj48@naver.com>
2025-01-08 17:40:32 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,77 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, M;
-    static int num, A, B;
-    static int[] parent;
-    static ArrayList<int[]> graph;
-
-    static int find(int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-        return parent[x] = find(parent[x]);
-    }
-
-    static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x != y) {
-            parent[y] = x;
-        }
-    }
+    static int N;
+    static String F, A;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        ArrayList<String> lst = new ArrayList<>();
+
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        graph = new ArrayList<>();
-
-        parent = new int[N+1];
-        for (int i=0; i<=N; i++) {
-            parent[i] = i;
+        for (int i=0; i<N; i++) {
+            F = st.nextToken();
+            lst.add(F);
         }
 
-        for (int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
-            num = Integer.parseInt(st.nextToken());
-            A = Integer.parseInt(st.nextToken());
-            B = Integer.parseInt(st.nextToken());
-            graph.add(new int[] {num, A, B});
-        }
+        A = br.readLine();
 
-        StringBuilder sb = new StringBuilder();
-
-        for (int[] temp : graph) {
-            int num = temp[0];
-            int a = temp[1];
-            int b = temp[2];
-
-            if (num == 0 && find(a) != find(b)) {
-                union(a, b);
-            } else if (num == 1) {
-                boolean isSame = true;
-                if (!check(a, b)) {
-                    isSame = false;
-                }
-                if (!isSame) {
-                    sb.append("NO").append('\n');
-                } else {
-                    sb.append("YES").append('\n');
-                }
+        for (String tempLst : lst) {
+            if (tempLst.equals(A)) {
+                cnt++;
             }
         }
-        System.out.println(sb);
-    }
-    static boolean check(int a, int b) {
-        if (find(a) != find(b)) {
-            return false;
-        } else {
-            return  true;
-        }
+        System.out.println(cnt);
     }
 }
---
+++ Algorithm/src/Solution/PrepareA_beakjun/JipHap.java
@@ -0,0 +1,81 @@
+// 1717. 집합의 표현 (유니온 파인드)
+package Solution.PrepareA_beakjun;
+
+import java.util.*;
+import java.io.*;
+
+public class JipHap {
+    static int N, M;
+    static int num, A, B;
+    static int[] parent;
+    static ArrayList<int[]> graph;
+
+    static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+
+        parent = new int[N+1];
+        for (int i=0; i<=N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            num = Integer.parseInt(st.nextToken());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            graph.add(new int[] {num, A, B});
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int[] temp : graph) {
+            int num = temp[0];
+            int a = temp[1];
+            int b = temp[2];
+
+            if (num == 0 && find(a) != find(b)) {
+                union(a, b);
+            } else if (num == 1) {
+                boolean isSame = true;
+                if (!check(a, b)) {
+                    isSame = false;
+                }
+                if (!isSame) {
+                    sb.append("NO").append('\n');
+                } else {
+                    sb.append("YES").append('\n');
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+    static boolean check(int a, int b) {
+        if (find(a) != find(b)) {
+            return false;
+        } else {
+            return  true;
+        }
+    }
+}
