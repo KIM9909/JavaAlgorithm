69f452a113dadbbd4be20862fa1f4a0722f19150
kimhongbeom <fkdldj48@naver.com>
2025-03-22 23:56:45 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -4,8 +4,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        System.out.println("강한친구 대한육군");
-        System.out.println("강한친구 대한육군");
+        System.out.println("I'm Sexy");
 
     }
 }
