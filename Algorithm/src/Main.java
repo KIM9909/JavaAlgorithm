da6a34cf10c93de58fae3fa85cef5a918585f533
kimhongbeom <fkdldj48@naver.com>
2025-03-23 22:34:27 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -1,10 +1,21 @@
+import java.math.BigInteger;
 import java.util.*;
 import java.io.*;
 
 public class Main {
+    static BigInteger N, M;
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        System.out.println("I'm Sexy");
+        N = new BigInteger(st.nextToken());
+        M = new BigInteger(st.nextToken());
+
+        if (N.equals(M)) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
 
     }
 }
