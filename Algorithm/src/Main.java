8d223c32fb2aa071b710f68215d559d9c6d71b76
김홍범 <fkdldj48@naver.com>
2025-06-14 10:49:44 +0900

    bronze

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -1,16 +1,20 @@
 import java.io.*;
 
 public class Main {
-    static int N;
+    static int total;
+    static int nineBooks = 0;
+    static int res;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
+        total = Integer.parseInt(br.readLine());
 
-        for (int i=0; i<N; i++) {
-            String word = br.readLine();
-
-            System.out.println(word.toLowerCase());
+        for (int i=0; i<9; i++) {
+            nineBooks += Integer.parseInt(br.readLine());
         }
+
+        res = total - nineBooks;
+
+        System.out.println(res);
     }
 }
