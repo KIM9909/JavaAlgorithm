7f6f602162e1aa4bf6a31ab64304ff2433cb94e6
김홍범 <fkdldj48@naver.com>
2025-06-20 20:13:28 +0900

    Programmers_Hash

---
+++ Algorithm/src/Solution/Programmers/Hash/PhoneNumbers.java
@@ -0,0 +1,25 @@
+package Solution.Programmers.Hash;
+// Lv.2 전화번호 목록
+
+import java.util.*;
+class PhoneNumbers {
+    public boolean solution(String[] phone_book) {
+        HashMap<String, Boolean> map = new HashMap();
+
+        for (String number : phone_book) {
+            map.put(number, true);
+        }
+
+        for (String number : phone_book) {
+            for (int i=1; i<number.length(); i++) {
+                String prefix = number.substring(0, i);
+
+                if (map.containsKey(prefix)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
