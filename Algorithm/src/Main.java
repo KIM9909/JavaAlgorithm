2a43677b09195f614010ae8a6f3a02fcf5bf6d6a
김홍범 <fkdldj48@naver.com>
2025-06-27 11:29:19 +0900

    1284. 집 주소

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,37 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int M, X, Y;
-    static int[] arr;
+    static String N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        M = Integer.parseInt(br.readLine());
-        arr = new int[4];
+        String line;
 
-        for (int i=1; i<=3; i++) {
-            if (i == 1) {
-                arr[i] = 1;
-            } else {
-                arr[i] = 0;
+        while (!(line = br.readLine()).equals("0")) {
+            N = line;
+            int width = 1;
+            for (int i=0; i<N.length(); i++) {
+                if (N.charAt(i) == '1') {
+                    width += 2;
+                } else if (N.charAt(i) == '0') {
+                    width += 4;
+                } else {
+                    width += 3;
+                }
+                width += 1;
             }
-        }
 
-        int temp;
-        for (int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
-            X = Integer.parseInt(st.nextToken());
-            Y = Integer.parseInt(st.nextToken());
-            temp = arr[X];
-            arr[X] = arr[Y];
-            arr[Y] = temp;
-        }
-
-        for (int i=0; i<4; i++) {
-            if (arr[i] == 1) {
-                System.out.println(i);
-            }
+            System.out.println(width);
         }
     }
 }
