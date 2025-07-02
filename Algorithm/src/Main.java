11bdf93708f5243dc9a29811ca06a2d69e45d61d
김홍범 <fkdldj48@naver.com>
2025-07-02 12:24:46 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,45 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[] boxSize, bookSize;
-    static int waste = 0;
+    static String hwan;
+    static String doctor;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        hwan = br.readLine();
+        doctor = br.readLine();
 
-        boxSize = new int[N];
-        bookSize = new int[M];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i=0; i<N; i++) {
-            boxSize[i] = Integer.parseInt(st.nextToken());
+        if (hwan.length() < doctor.length()) {
+            System.out.println("no");
+        } else {
+            System.out.println("go");
         }
-
-        st = new StringTokenizer(br.readLine());
-        for (int i=0; i<M; i++) {
-            bookSize[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int boxIndex = 0;
-        int bookIndex = 0;
-        while (boxIndex < N && bookIndex < M) {
-            if (boxSize[boxIndex] - bookSize[bookIndex] >= 0) {
-                boxSize[boxIndex] -= bookSize[bookIndex];
-                bookIndex ++;
-            } else {
-                boxIndex ++;
-            }
-        }
-
-        for (int i=0; i<N; i++) {
-            waste += boxSize[i];
-        }
-
-        System.out.println(waste);
     }
 }
