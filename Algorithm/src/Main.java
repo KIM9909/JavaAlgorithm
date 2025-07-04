46ee0346066c271da73613a596ec4a75892d4e57
김홍범 <fkdldj48@naver.com>
2025-07-04 12:06:03 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,18 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static String hwan;
-    static String doctor;
+    static String word;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        hwan = br.readLine();
-        doctor = br.readLine();
+        word = br.readLine();
 
-        if (hwan.length() < doctor.length()) {
-            System.out.println("no");
-        } else {
-            System.out.println("go");
+        String res = "";
+
+        for (int i=0; i<word.length(); i++) {
+            char w = word.charAt(i);
+
+            if (Character.isUpperCase(w)) {
+                res += Character.toLowerCase(w);
+            } else {
+                res += Character.toUpperCase(w);
+            }
         }
+
+        System.out.println(res);
     }
 }
