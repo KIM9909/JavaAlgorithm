d37adcf51a9da885c537729824f1450546ea8f95
김홍범 <fkdldj48@naver.com>
2025-07-11 12:13:15 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,20 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int T;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        T = Integer.parseInt(br.readLine());
-
-        for (int i=0; i<T; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            System.out.println("Case " + (i+1) + ": " + (x + y));
-        }
-
+        System.out.println("고려대학교");
     }
 }
