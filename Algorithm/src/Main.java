8c3db5791d7556b6768a1a69270d1917dc18173b
김홍범 <fkdldj48@naver.com>
2025-07-14 20:11:57 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -3,6 +3,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("A");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        while (N != 0) {
+            StringBuilder sb = new StringBuilder();
+            for (int i=0; i<N; i++) {
+                sb.append("*");
+            }
+            System.out.println(sb.toString());
+            N -= 1;
+        }
     }
 }
