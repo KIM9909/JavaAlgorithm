20d7533ded3d53dda056cf35ffada9fb26e0f95e
김홍범 <fkdldj48@naver.com>
2025-07-18 11:07:49 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,35 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int start, end, ball;
-    static int[] res;
+    static int R1, S;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        R1 = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
 
-        res = new int[N+1];
-
-        for (int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
-            start = Integer.parseInt(st.nextToken());
-            end = Integer.parseInt(st.nextToken());
-            ball = Integer.parseInt(st.nextToken());
-
-            for (int j=start; j<=end; j++) {
-                res[j] = ball;
-            }
-        }
-
-        for (int i=1; i<=N; i++) {
-            sb.append(res[i]).append(" ");
-        }
-
-        System.out.println(sb);
+        System.out.println(S * 2 - R1);
     }
 }
