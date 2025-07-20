9ffb548939afcdc74cbdd100758f4f29491f6f6e
김홍범 <fkdldj48@naver.com>
2025-07-20 12:22:07 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,15 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int R1, S;
+    static int info, math, science, english;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int maxScore = Integer.MIN_VALUE;
+        int sum = 0;
         StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        R1 = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
+        for (int i=0; i<2; i++) {
+            st = new StringTokenizer(br.readLine());
+            info = Integer.parseInt(st.nextToken());
+            math = Integer.parseInt(st.nextToken());
+            science = Integer.parseInt(st.nextToken());
+            english = Integer.parseInt(st.nextToken());
+            sum += info;
+            sum += math;
+            sum += science;
+            sum += english;
 
-        System.out.println(S * 2 - R1);
+            maxScore = Math.max(maxScore, sum);
+
+            sum = 0;
+        }
+
+        System.out.println(maxScore);
     }
 }
