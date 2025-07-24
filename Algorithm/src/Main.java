b85aa05965fb0515abad8fbe4525bd4dc1ff2fd3
김홍범 <fkdldj48@naver.com>
2025-07-24 10:57:23 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,29 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static String name;
-    static int age, weight;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        String line;
 
-        while ((line = br.readLine()) != null) {
-            st = new StringTokenizer(line);
-            name = st.nextToken();
-            age = Integer.parseInt(st.nextToken());
-            weight = Integer.parseInt(st.nextToken());
+        int sum = 0;
 
-            if (name.equals("#")) {
-                break;
-            }
+        for (int i=0; i<4; i++) {
+            int time = Integer.parseInt(br.readLine());
 
-            if (age > 17 || weight >= 80) {
-                System.out.println(name + " Senior");
-            } else {
-                System.out.println(name + " Junior");
-            }
+            sum += time;
         }
-        br.close();
+
+        System.out.println(sum / 60);
+        System.out.println(sum % 60);
     }
 }
