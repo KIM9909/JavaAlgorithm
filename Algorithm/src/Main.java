152222d272511e872518db4b8023dd1996fef40b
김홍범 <fkdldj48@naver.com>
2025-07-26 10:45:12 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,18 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static int cnt = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int sum = 0;
+        N = Integer.parseInt(br.readLine());
+        String strN = String.valueOf(N);
 
-        for (int i=0; i<4; i++) {
-            int time = Integer.parseInt(br.readLine());
+        do {
+            if (strN.length() == 1) {
+                strN = "0" + strN;
+            }
 
-            sum += time;
-        }
+            int sum = Integer.parseInt(String.valueOf(strN.charAt(0))) + Integer.parseInt(String.valueOf(strN.charAt(1)));
+            int last = Integer.parseInt(String.valueOf(strN.charAt(1)));
 
-        System.out.println(sum / 60);
-        System.out.println(sum % 60);
+            strN = String.valueOf(last) + (String.valueOf(sum).length() == 1 ? String.valueOf(sum) : String.valueOf(String.valueOf(sum).charAt(1)));
+            cnt ++;
+        } while (Integer.parseInt(strN) != N);
+
+        System.out.println(cnt);
     }
 }
