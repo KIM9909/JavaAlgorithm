566e86a450844f7495ab33311338d9aeb65dffcd
김홍범 <fkdldj48@naver.com>
2025-07-27 10:48:40 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,26 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int cnt = 0;
+    static int month, day;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        String strN = String.valueOf(N);
+        month = Integer.parseInt(br.readLine());
+        day = Integer.parseInt(br.readLine());
 
-        do {
-            if (strN.length() == 1) {
-                strN = "0" + strN;
-            }
+        if (month == 2 && day == 18) {
+            System.out.println("Special");
+        }
 
-            int sum = Integer.parseInt(String.valueOf(strN.charAt(0))) + Integer.parseInt(String.valueOf(strN.charAt(1)));
-            int last = Integer.parseInt(String.valueOf(strN.charAt(1)));
+        if (month < 2 || (month == 2 && day < 18)) {
+            System.out.println("Before");
+        }
 
-            strN = String.valueOf(last) + (String.valueOf(sum).length() == 1 ? String.valueOf(sum) : String.valueOf(String.valueOf(sum).charAt(1)));
-            cnt ++;
-        } while (Integer.parseInt(strN) != N);
-
-        System.out.println(cnt);
+        if (month > 2 || (month == 2 && day > 18)) {
+            System.out.println("After");
+        }
     }
 }
