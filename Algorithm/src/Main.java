41a8addcefa91414437f41f1ac06ab79db11e9bf
김홍범 <fkdldj48@naver.com>
2025-07-29 10:41:33 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,23 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int month, day;
+    static int N;
+    static int YCost, MCost;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        month = Integer.parseInt(br.readLine());
-        day = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        YCost = 0;
+        MCost = 0;
 
-        if (month == 2 && day == 18) {
-            System.out.println("Special");
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            int callTime = Integer.parseInt(st.nextToken());
+
+            if (callTime < 30) {
+                YCost += 10;
+            } else if (callTime >= 30 && callTime % 30 >= 0) {
+                YCost += (callTime / 30) * 10 + 10;
+            }
+
+            if (callTime < 60) {
+                MCost += 15;
+            } else if (callTime >= 60 && callTime % 60 >= 0) {
+                MCost += (callTime / 60) * 15 + 15;
+            }
         }
 
-        if (month < 2 || (month == 2 && day < 18)) {
-            System.out.println("Before");
-        }
-
-        if (month > 2 || (month == 2 && day > 18)) {
-            System.out.println("After");
+        if (MCost < YCost) {
+            System.out.println("M " + MCost);
+        } else if (MCost == YCost) {
+            System.out.println("Y " + "M " + MCost);
+        } else {
+            System.out.println("Y " + YCost);
         }
     }
 }
