a24c72da2ac676577f4f8d676e9c37a29144ea94
김홍범 <fkdldj48@naver.com>
2025-07-30 10:22:57 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,39 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int YCost, MCost;
+    static int ascii;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
-        YCost = 0;
-        MCost = 0;
+        ascii = br.readLine().charAt(0);
 
-        st = new StringTokenizer(br.readLine());
-        for (int i=0; i<N; i++) {
-            int callTime = Integer.parseInt(st.nextToken());
-
-            if (callTime < 30) {
-                YCost += 10;
-            } else if (callTime >= 30 && callTime % 30 >= 0) {
-                YCost += (callTime / 30) * 10 + 10;
-            }
-
-            if (callTime < 60) {
-                MCost += 15;
-            } else if (callTime >= 60 && callTime % 60 >= 0) {
-                MCost += (callTime / 60) * 15 + 15;
-            }
-        }
-
-        if (MCost < YCost) {
-            System.out.println("M " + MCost);
-        } else if (MCost == YCost) {
-            System.out.println("Y " + "M " + MCost);
-        } else {
-            System.out.println("Y " + YCost);
-        }
+        System.out.println(ascii);
     }
 }
