b90fc65b31c3315d55177a0f4f080ce7d36fdb52
김홍범 <fkdldj48@naver.com>
2025-08-04 09:59:38 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,51 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, K;
-    static int[] front, back;
+    static String word;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        word = br.readLine();
 
-        front = new int[N];
-        back = new int[N];
-
-        int frontO = 0;
-        int backO = 0;
-        int frontX = 0;
-        int backX = 0;
-
-        for (int i=0; i<M; i++) {
-            front[i] = 1;
+        if (word.equals("N") || word.equals("n")) {
+            System.out.println("Naver D2");
+        } else {
+            System.out.println("Naver Whale");
         }
-
-        for (int i=0; i<K; i++) {
-            back[i] = 1;
-        }
-
-        for (int num : front) {
-            if (num == 1) {
-                frontO ++;
-            } else {
-                frontX ++;
-            }
-        }
-
-        for (int num : back) {
-            if (num == 1) {
-                backO ++;
-            } else {
-                backX ++;
-            }
-        }
-        int cntO = Math.min(frontO, backO);
-        int cntX = Math.min(frontX, backX);
-
-        System.out.println(cntO + cntX);
     }
 }
