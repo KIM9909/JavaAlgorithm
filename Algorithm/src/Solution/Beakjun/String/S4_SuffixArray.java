1877cac042160a7c1b24ddedb8d2815cfe9ac3d7
김홍범 <fkdldj48@naver.com>
2025-08-12 12:06:32 +0900

    11656. 접미사 배열

---
+++ Algorithm/src/Solution/Beakjun/String/S4_SuffixArray.java
@@ -0,0 +1,32 @@
+package Solution.Beakjun.String;
+// 11656. 접미사 배열
+
+import java.util.*;
+import java.io.*;
+public class S4_SuffixArray {
+    static String S;
+    static String[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb;
+
+        S = br.readLine();
+        arr = new String[S.length()];
+
+        for (int i=0; i<S.length(); i++) {
+            sb = new StringBuilder();
+            for (int j=i; j<S.length(); j++) {
+                sb.append(S.charAt(j));
+            }
+            arr[i] = sb.toString();
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            return a.compareTo(b);
+        });
+
+        for (String s : arr) {
+            System.out.println(s);
+        }
+    }
+}
