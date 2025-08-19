36e40b15613085f44ac665f7ee024fc111951dd6
김홍범 <fkdldj48@naver.com>
2025-08-19 12:47:27 +0900

    1100. 하얀 칸

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,17 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int T, A, B;
+    static String[] arr;
+    static int cnt = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        T = Integer.parseInt(br.readLine());
+        arr = new String[8];
 
-        for (int t=0; t<T; t++) {
-            st = new StringTokenizer(br.readLine());
-            A = Integer.parseInt(st.nextToken());
-            B = Integer.parseInt(st.nextToken());
+        for (int i=0; i<8; i++) {
+            arr[i] = br.readLine();
         }
+
+        for (int i=0; i<8; i++) {
+            for (int j=0; j<8; j++) {
+                if (i % 2 == 0) {
+                    if (j % 2 == 0 && arr[i].charAt(j) == 'F') {
+                        cnt ++;
+                    }
+                } else {
+                    if (j % 2 != 0 && arr[i].charAt(j) == 'F') {
+                        cnt ++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(cnt);
     }
 }
