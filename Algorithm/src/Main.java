3a265521299ab8a4780f11377b188186475e3714
김홍범 <fkdldj48@naver.com>
2025-08-29 08:53:59 +0900

    2669. 직사각형 네개의 합집합의 면적 구하기

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,25 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static String N;
-    static Integer[] arr;
+    static int[][] arr;
+    static int x1,y1,x2,y2;
+    static int cnt = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        N = br.readLine();
-        arr = new Integer[N.length()];
+        arr = new int[101][101];
 
-        for (int i=0; i<N.length(); i++) {
-            arr[i] = Integer.parseInt(String.valueOf(N.charAt(i)));
+        for (int i=0; i<4; i++) {
+            st = new StringTokenizer(br.readLine());
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+
+            for (int x=x1; x<x2; x++) {
+                for (int y=y1; y<y2; y++) {
+                    arr[x][y] = 1;
+                }
+            }
         }
 
-        Arrays.sort(arr, Collections.reverseOrder());
-
-        for (int num : arr) {
-            sb.append(num);
+        for (int i=1; i<arr.length; i++) {
+            for (int j=1; j<arr[0].length; j++) {
+                if (arr[i][j] == 1) {
+                    cnt ++;
+                }
+            }
         }
 
-        System.out.println(sb.toString());
+        System.out.println(cnt);
     }
 }
