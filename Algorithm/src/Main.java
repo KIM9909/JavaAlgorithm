78ced9f334e4f661f2a2df3b44431c899daf8296
김홍범 <fkdldj48@naver.com>
2025-09-14 11:28:13 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,23 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int L;
+    static String id;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        L = Integer.parseInt(br.readLine());
-        int num = 5;
-        int cnt = 0;
+        id = br.readLine();
 
-        while (L > 0) {
-            if (L == 0) {
-                break;
-            }
-            cnt += L/num;
-            L %= num;
-            num --;
-        }
-
-        System.out.println(cnt);
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + id + "::fan:");
+        System.out.println(":fan::fan::fan:");
     }
 }
