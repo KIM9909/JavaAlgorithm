f01aec55b94ed3c050d540e85713385dcc44f511
김홍범 <fkdldj48@naver.com>
2025-09-17 08:22:59 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,19 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] arr;
+    static int t;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
 
-        arr = new int[] {1, 1, 2, 2, 2, 8};
+        t = Integer.parseInt(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        for (int i=0; i<arr.length; i++) {
-            sb.append(arr[i] - Integer.parseInt(st.nextToken())).append(" ");
+        for (int i=0; i<t; i++) {
+            st = new StringTokenizer(br.readLine());
+            System.out.println(Long.parseLong(st.nextToken()) + Long.parseLong(st.nextToken()));
         }
-
-        System.out.println(sb.toString());
     }
 }
