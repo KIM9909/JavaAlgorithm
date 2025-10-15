b15f3ff15d46cf94127e3382364ce03951f06ea6
김홍범 <fkdldj48@naver.com>
2025-10-15 11:08:52 +0900

    G5_아우으_우아으이야

--- Algorithm/src/Solution/Beakjun/Sweeping/G5_아우으_우아으이야.java
+++ Algorithm/src/Solution/Beakjun/Sweeping/G5_아우으_우아으이야.java
@@ -2,7 +2,7 @@ package Solution.Beakjun.Sweeping;
 
 import java.io.*;
 import java.util.*;
-public class G5_아우으_우아으아야 {
+public class G5_아우으_우아으이야 {
     static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
