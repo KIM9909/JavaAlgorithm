420870ade0efbcbf2e5ef40f43986a5d892bdb2b
김홍범 <fkdldj48@naver.com>
2025-11-04 11:40:35 +0900

    1

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -1,19 +1,34 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-    static int A, B, C;
+    static int T;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        st = new StringTokenizer(br.readLine());
-        A = Integer.parseInt(st.nextToken());
-        B = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(br.readLine());
 
-        int res = B / A * 3;
+        for (int t=0; t<T; t++) {
+            String line = br.readLine();
 
-        System.out.println(res * C);
+            String[] words = line.split(" ");
+
+            for (String word : words) {
+                if (word.length() == 1) {
+                    sb.append(word).append(" ");
+                } else {
+                    StringBuilder str = new StringBuilder();
+                    for (int i=word.length()-1; i>=0; i--) {
+                        str.append(word.charAt(i));
+                    }
+
+                    sb.append(str).append(" ");
+                }
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
     }
 }
