120d32cc87e97bb5b3cb07b17f8efe42f7e3bc6c
김홍범 <fkdldj48@naver.com>
2025-11-13 16:44:48 +0900

    S5 정렬적인 정렬

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,50 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] arr;
-
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        arr = new int[5];
+        N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 5; i++) {
+        for (int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        int result = Integer.MAX_VALUE;
-
-        for (int i = 0; i < 5; i++) {
-            for (int j = i + 1; j < 5; j++) {
-                for (int k = j + 1; k < 5; k++) {
-                    int lcm1 = lcm(arr[i], arr[j]);
-                    int lcm2 = lcm(lcm1, arr[k]);
-
-                    result = Math.min(result, lcm2);
-                }
-            }
+        for (int a : arr) {
+            sb.append(a).append(" ");
         }
 
-        System.out.println(result);
-    }
-
-    // 최대공약수
-    static int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-
-    // 최소공배수
-    static int lcm(int a, int b) {
-
-        return (a * b) / gcd(a, b);
+        System.out.println(sb);
     }
 }
